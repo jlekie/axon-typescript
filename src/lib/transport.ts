3bc5d07d30fde40ff036757ee0e8f2c34cc4d37b
Joe Lekie <joe.lekie@greenheck.com>
2019-05-20 16:02:28 -0500

    Checkpoint

--- src/lib/transport.ts
+++ src/lib/transport.ts
@@ -1,27 +1,53 @@
 export interface IReceivedData {
     readonly data: Buffer;
-    readonly metadata: Map<string, Buffer>;
+    readonly metadata: Record<string, Buffer>;
 }
 export class ReceivedData implements IReceivedData {
     public readonly data: Buffer;
-    public readonly metadata: Map<string, Buffer>;
+    public readonly metadata: Record<string, Buffer>;
 
-    public constructor(data: Buffer, metadata?: Map<string, Buffer>) {
+    public constructor(data: Buffer, metadata?: Record<string, Buffer>) {
         this.data = data;
 
-        this.metadata = new Map();
-        if (metadata) {
-            for (const kv of metadata)
-                this.metadata.set(kv[0], kv[1]);
-        }
+        if (metadata)
+            this.metadata = { ...metadata };
+        else
+            this.metadata = {};
+    }
+}
+
+export interface IReceivedTaggedData {
+    readonly tag: string;
+    readonly data: Buffer;
+    readonly metadata: Record<string, Buffer>;
+}
+
+export class ReceivedTaggedData implements IReceivedTaggedData {
+    public readonly tag: string;
+    public readonly data: Buffer;
+    public readonly metadata: Record<string, Buffer>;
+
+    public constructor(tag: string, data: Buffer, metadata?: Record<string, Buffer>) {
+        this.tag = tag;
+        this.data = data;
+
+        if (metadata)
+            this.metadata = { ...metadata };
+        else
+            this.metadata = {};
     }
 }
 
 export interface ITransport {
-    send(data: Buffer, metadata: Map<string, Buffer>): Promise<void>;
-    receive(): Promise<IReceivedData>;
+    send(data: Buffer, metadata: Record<string, Buffer>): Promise<void>;
+    sendTagged(messageId: string, data: Buffer, metadata: Record<string, Buffer>): Promise<void>;
 
-    sendAndReceive(data: Buffer, metadata: Map<string, Buffer>): Promise<() => Promise<IReceivedData>>;
+    receive(): Promise<IReceivedData>;
+    receiveTagged(messageId: string): Promise<IReceivedData>;
+
+    receiveBufferedTagged(): Promise<IReceivedTaggedData>;
+
+    sendAndReceive(data: Buffer, metadata: Record<string, Buffer>): Promise<() => Promise<IReceivedData>>;
 }
 export interface IServerTransport extends ITransport {
     listen(): Promise<void>;
@@ -33,9 +59,15 @@ export interface IClientTransport extends ITransport {
 }
 
 export abstract class ATransport implements ITransport {
-    public abstract send(data: Buffer, metadata: Map<string, Buffer>): Promise<void>;
+    public abstract send(data: Buffer, metadata: Record<string, Buffer>): Promise<void>;
+    public abstract sendTagged(messageId: string, data: Buffer, metadata: Record<string, Buffer>): Promise<void>;
+
     public abstract receive(): Promise<IReceivedData>;
-    public abstract sendAndReceive(data: Buffer, metadata: Map<string, Buffer>): Promise<() => Promise<IReceivedData>>;
+    public abstract receiveTagged(messageId: string): Promise<IReceivedData>;
+
+    public abstract receiveBufferedTagged(): Promise<IReceivedTaggedData>;
+
+    public abstract sendAndReceive(data: Buffer, metadata: Record<string, Buffer>): Promise<() => Promise<IReceivedData>>;
 }
 export abstract class AServerTransport extends ATransport implements IServerTransport {
     public abstract listen(): Promise<void>;
