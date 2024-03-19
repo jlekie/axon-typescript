0595b006562a8005e11adb77dc1cd97eae321ee0
Joe Lekie <joe.lekie@umbrexus.com>
2024-03-19 08:18:02 -0500

    removed LZ4 dependencies

--- lz4.d.ts
+++
@@ -1,2 +0,0 @@
-export function encode(data: Buffer): Buffer;
-export function decode(data: Buffer): Buffer;
--- src/lz4-asm.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'lz4-asm' {
-    export function compress(data: Buffer): Buffer;
-    export function decompress(data: Buffer): Buffer;
-}
